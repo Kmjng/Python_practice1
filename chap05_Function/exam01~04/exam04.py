89bc04eba8a7503c336c456c4cf3b091049448f7
Minjeong Kim <ppz7227@naver.com>
2024-03-07 18:11:16 +0900

    Update exam04.py

--- chap05_Function/exam01~04/exam04.py
+++ chap05_Function/exam01~04/exam04.py
@@ -5,16 +5,15 @@
   <출력 결과>
   tot = [12.5, 7, 22.3]
 '''
-
-# tot 함수 정의 
-def tot(x):
-    return sum(x)
-
-# tot 함수를 인수로 받는 함수 정의 
-def my_func(func, datas):
-     pass #내용 채우기
-
-
 # dataset
 datas = [[2,4.5,6], [3,4], [5,8.3,9]]
+# tot 함수 정의 
+def tot(x): 
+    return sum(x)
+# tot 함수를 인수로 받는 함수 정의 
+def my_func(func,datas):
+    lst = [func(i) for i in datas]
+    return lst 
+
+print(my_func(tot, datas))
 
