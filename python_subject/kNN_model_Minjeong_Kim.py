a75a149aad2a21440d83f4f3d3b7635c14c72ab4
kmjng <ppz7227@naver.com>
2024-03-19 20:48:42 +0900

    Update kNN_model_Minjeong_Kim.py

--- python_subject/kNN_model_Minjeong_Kim.py
+++ python_subject/kNN_model_Minjeong_Kim.py
@@ -20,66 +20,46 @@ print('q :', q)
 print('p : ', p)
 print('-'*60)
 
-'''
+
 # 단계2. kNN 분류기(클래스 정의) : 데이터(필드) + 알고리즘(메서드)
 class kNN :    
     def __init__(self, q, p):
         self.q = q 
         self.p = p 
-    def distances(self):
-        self.dist = [] 
-        for i in range(len(q)):
-            
-            self.x1 = (q[i][0]-p[0])**2
-            self.y1 = (q[i][1]-p[1])**2
-            self.d1 = (self.x1+self.y1)**(1/2)
-            self.dist.append(self.d1)
-            self.dist=sorted(self.dist, reverse=False)
-            
-        return self.dist
-    def axis(self):
-        return q    
-    def k(self,j): #최근접 이웃 j개 거리 추출 메소드
-        return self.dist[:j]
-''' 
-# 단계2. kNN 분류기(클래스 정의) : 데이터(필드) + 알고리즘(메서드)
-class kNN :    
-    def __init__(self, q, p):
-        self.q = q 
-        self.p = p 
-    def distances(self):
         
-        self.real = {}
+        
+    def distances(self, j):
+        self.real = {}  # q 좌표들과 p까지의 거리를 매칭하기 위한 딕셔너리
         self.dist =[]
         for i in range(len(q)):
         
-            self.x1 = (q[i][0]-p[0])**2
-            self.y1 = (q[i][1]-p[1])**2
+            self.x1 = (self.q[i][0]-self.p[0])**2
+            self.y1 = (self.q[i][1]-self.p[1])**2
             self.d1 = (self.x1+self.y1)**(1/2)
-            self.real[i]= self.d1 # {0:~~} # 딕셔너리에 순서 저장하고 
-            self.dist.append(self.d1) # 리스트에 거리 저장 
+
+            self.real[i]= self.d1 # {0:~~} # 딕셔너리에 q 좌표순서 저장 
+            self.dist.append(self.d1) # 리스트에 '거리' 저장 
             self.dist = sorted(self.dist, reverse=False) #value로 정렬
             
-        return self.dist
+        return self.dist[:j] #  j 까지의 최근접거리 반환
     
-    def k(self,j): #최근접 이웃 j개 거리 추출 메소드
-        real_d = []
-        self.dist2 = sorted(self.dist, key = self.dist.get, reverse=False) # key 반환 
-        
-        for i in self.dist2[:j]: #[1,7,3]
-            real_d.append(q[i])
-        return self.dist3, real
+    def k(self,j): #최근접 이웃 j개 거리좌표 추출 메소드
+        self.real_d = [] # 좌표 담을 리스트
+        self.real_sorted = sorted(self.real, key = self.real.get, reverse=False) # key 반환 
+        for i in self.real_sorted[:j]: #[1,7,3]
+            self.real_d.append(self.q[i])
+        return self.real_d
 
 
 
 # 단계3. kNN 객체 생성             
 knn = kNN(q, p) # (적용데이터셋, 기준데이터셋)
-knn.distances()
+knn.distances(3)
 knn.k(3)
-
-print(f'k1 ->{knn.k(1)}')
-print(f'k3 ->{knn.k(3)}')
-print(f'k5 ->{knn.k(5)}')
+print(knn.k(1))
+print('k1 ->{0}\nreal data: {1}'.format(knn.distances(1),knn.k(1)))
+print('k3 ->{0}\nreal data: {1}'.format(knn.distances(3),knn.k(3)))
+print('k5 ->{0}\nreal data: {1}'.format(knn.distances(5),knn.k(5)))
 
 
     
